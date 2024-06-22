b3a6a1f980447621793487ce77f5d8be80b9eadd
Aman <349aman349@gmail.com>
2024-06-22 21:18:30 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); //req.body
-const PORT=process.env.PORT||3000;
+const PORT=process.env.PORT || 3000;
 
 app.get("/", function (req, res) {
   res.send("Welcome to our Hotel");
