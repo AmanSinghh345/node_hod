8c246a5859650c0e70c64b11afe4a663d2a0f3db
Aman <349aman349@gmail.com>
2024-06-22 15:28:35 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 const mongoose=require('mongoose');
+require('dotenv').config();
+
 //Define the MongoDB connection URL
-const mongoURL='mongodb://localhost:27017/hotels';
+//const mongoURL='mongodb://localhost:27017/hotels';
+//const mongoURL='mongodb+srv://amansinghrajput15022004:W9B4LF2t5WKGU7Tv@cluster0.r4spvvb.mongodb.net/';
+//const mongoURL=process.env.MONGODB_URL_LOCAL
+const mongoURL=process.env.MONGODB_URL;
+
 //Set up MongoDB connetion
 mongoose.connect(mongoURL,{
     useNewUrlParser:true,  //mandatory
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require('dotenv').config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); //req.body
+const PORT=process.env.PORT||3000;
 
 app.get("/", function (req, res) {
   res.send("Welcome to our Hotel");
@@ -97,6 +99,8 @@ const menuItemRoutes = require("./routes/menuItemRoutes");
 app.use("/person", personRoutes);
 app.use("/menu", menuItemRoutes);
 
-app.listen(3000, () => {
+
+
+app.listen(PORT, () => {
   console.log("listening to port 3000");
 });
